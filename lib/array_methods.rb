2960f0b0fff815bbb4dc6cc07a4acedfff9b84d9
Trevor Jones <tjones009@yahoo.com>
2020-02-26 21:08:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,12 +9,12 @@ end
 
 
 def find_max_value(array)
-  x = array[0]
-  array.length.times { |index|
+  x = 0
+  array.length.times do |index|
     if array[index] > x
-      x=array[index]
+      x = array[index]
     end
-  }
+  end
   x
 end
 
